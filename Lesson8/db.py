0fcec60e21a22cf65cd61894d3929f60ce6ec705
satskyi1991 <59996139+satskyi1991@users.noreply.github.com>
2020-04-05 20:25:56 +0200

    Create db.py

---
+++ Lesson8/db.py
@@ -0,0 +1,20 @@
+POSTS = [
+    {
+        'id': 1,
+        'title': 'Coronovirus',
+        'body': 'SOME INFO ABOUT CORONAVIRUS....',
+        'author': 'Lehnon'
+    },
+    {
+        'id': 2,
+        'title': '1984',
+        'body': 'text',
+        'author': 'Orwell'
+    },
+    {
+        'id': 3,
+        'title': 'Matrix',
+        'body': 'Lorem ipsum',
+        'author': 'Orwell'
+    }
+]
