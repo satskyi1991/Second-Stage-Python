96cff50044dc8dda421158eb4a70a9ac3035fa95
satskyi1991 <59996139+satskyi1991@users.noreply.github.com>
2020-04-05 19:58:46 +0200

    Create main.py

---
+++ Lesson3/main.py
@@ -0,0 +1,43 @@
+class A:
+
+    def __init__(self, a_variable,
+                 b_variable, c_variable,
+                 d_variable, e_variable):
+        pass
+
+    def move(self):
+        pass
+
+    def drive(self):
+        key = "12312312"
+        print("putting key")
+
+        print("Press gas")
+
+    def is_true(self):
+        return True
+
+
+def test_pep(arg1, arg2, arg3):
+    pass
+
+
+test_pep(1, 2, 3)
+
+test_pep(1, 2, 3)
+
+dict_ = {
+    1: 2,
+    3: 4
+}
+
+a = 0
+
+test_pep(arg2=3, arg1=2, arg3=6)
+
+dict(
+    arg1=1,
+    arg2=2,
+    arg3=3,
+    arg4=4
+)
