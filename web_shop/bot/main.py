00767457d65a9a96b7af118dd66ee95675e603f8
satskyi1991 <59996139+satskyi1991@users.noreply.github.com>
2020-04-16 20:28:41 +0200

    Create main.py

---
+++ web_shop/bot/main.py
@@ -0,0 +1,115 @@
+from telebot import TeleBot
+from .config import TOKEN
+from .keyboards import START_KB
+from telebot.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Update
+)
+
+from ..db.models import (
+    Customer,
+    Category,
+    News,
+    Product
+)
+
+from flask import Flask
+from flask import request, abort
+
+
+bot = TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['Post'])
+def process_method():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get.data().decode('ulf-8')
+        update  = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(status = 403)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    kb = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    buttons = [KeyboardButton(value) for value in START_KB.values()]
+    kb.add(*buttons)
+
+    bot.send_message(
+        message.chat.id,
+        'Greetings!',
+        reply_markup=kb
+    )
+
+
+@bot.message_handler(func=lambda message: message.text == START_KB['categories'])
+def get_categories(message):
+    kb = InlineKeyboardMarkup(row_width=2)
+    categories = Category.get_root()
+    buttons  = [InlineKeyboardButton(text = category.title, callback_data = f'category{category.id}')for category in categories]
+    kb.add(*buttons)
+    bot.send_massege(
+        message.chat.id,
+        "Выберете категорию",
+        reply_marckup = True
+    )
+
+@bot.callback_query_handler(func = lambda call:call.data.startswith('category'))
+def category_handler(call):
+    category_id = ''.join(call.data.split('_')[1::])
+    category = Category.objects.get(id = category_id)
+    kb = InlineKeyboardMarkup(row_width = 2)
+    if category.subcategories:
+        categories = category.subcategories
+        buttons  = [InlineKeyboardButton(text = category.title,
+                    callback_data = f'category{category.id}')for category in category.subcategories]
+
+        kb.add(*buttons)
+        bot.edit_message_text(
+            category.title,
+            chat_id = call.message.chat.id,
+            message_id  = call.message.message_id,
+            reply_markup = kb
+        )
+
+    elif category.is_leaf:
+        for product in category.products:
+            button = InlineKeyboardButton(text = 'Добавить в карзину',
+                  callback_data = f'product_{product.id}')
+            kb.keyboard = [[button.to_dict()]]
+            
+            bot.send_photo(
+                call.massege.chat.id,
+                product.image.read(),
+                caption = product.description,
+                disable_notification = True,
+                reply_markup = kb
+            )
+
+# @bot.callback_query_handler(func = lambda call: call.data.startswith('product'))
+# def add
+
+@bot.message_handler(func=lambda message: message.text == START_KB['news'])
+def get_news(message):
+    pass
+
+@bot.message_handler(func=lambda message: message.text == START_KB['discount_products'])
+def get_discount_products(message):
+    pass
+
+def set_webhook():
+    import time
+    bot.remove_webhook()
+    time.sleep(1)
+    bot.set_webhook(
+        url = 'https://34.89.168.121/tg',
+        certificate = open('webhook_cert.pem', 'r')
+    )
+
