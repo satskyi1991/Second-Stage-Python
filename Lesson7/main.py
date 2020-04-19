c431f6ccc457eb49d81d6edbf8680e5f8e47807a
satskyi1991 <59996139+satskyi1991@users.noreply.github.com>
2020-04-19 11:15:01 +0200

    Update main.py

--- Lesson7/main.py
+++ Lesson7/main.py
@@ -1,16 +1,32 @@
+
 import sqlite3
+
 conn = sqlite3.connect('employees.db')
 cursor = conn.cursor()
 
+# value = input('enter value')
+# cursor.execute(
+#     f"""
+#     DELETE FROM emloyee
+#     WHERE emloyee.id > 7
+#     """
+# )
+# # conn.commit()
 
 cursor.execute(
     f"""
-    INSERT INTO employee ('first_name', 'surname', 'salary')
-    VALUES ('Anton', 'Polyakov', 1000000)
-    """,
+    INSERT INTO emloyee('first_name', 'Surname', 'salary')
+    VALUES ('Illia', 'Satskyi', '3300')
+    """
 )
 conn.commit()
 
+# INSERT INTO emloyee('first_name', 'Surname', 'salary')
+# VALUES ('Illia', 'Satskyi', '3300')
+# SELECT * FROM emloyee
+# WHERE salary > ?
 for data in cursor:
     print(data)
+
+#cursor.fetchone()
 conn.close()
