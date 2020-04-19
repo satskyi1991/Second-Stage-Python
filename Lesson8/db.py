8546a17473b0908246a443cb59e39e78186da3d5
satskyi1991 <59996139+satskyi1991@users.noreply.github.com>
2020-04-19 20:50:54 +0200

    Add files via upload

---
+++ Lesson8/db.py
@@ -0,0 +1,20 @@
+POSTS = [
+    {
+        'id': 1,
+        'title': 'Coronavirus',
+        'body': 'Some info about coronavirus',
+        'author': 'Lennon',
+    },
+    {
+        'id': 2,
+        'title': '1984',
+        'body': 'Some info',
+        'author': 'Lennon',
+    },
+    {
+        'id': 3,
+        'title': '1984',
+        'body': 'Some info about coronavirus!!',
+        'author': 'Lennon',
+    }
+]
